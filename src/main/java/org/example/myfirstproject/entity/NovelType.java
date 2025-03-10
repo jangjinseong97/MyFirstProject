aa9ce220bedf5fe981bb5fc20587a25254b291cc
jin <hn04223@gmail.com>
2025-03-10 18:03:30 +0900

    25/03/10 entity, converter, errorCode 등등

--- src/main/java/org/example/myfirstproject/entity/NovelType.java
+++ src/main/java/org/example/myfirstproject/entity/NovelType.java
@@ -9,7 +9,12 @@ import org.example.myfirstproject.config.relationenum.EnumMapperType;
 @Getter
 @RequiredArgsConstructor
 public enum NovelType implements EnumMapperType {
-    FREE("01", "자유")
+    SERIAL_NOVEL("01", "연재"),
+    DELAY_NOVEL("02", "연재지연"),
+    DROP_NOVEL("03", "연재중단"),
+    PAUSED_NOVEL("04", "휴재"),
+    END_NOVEL("05", "완결"),
+    DRAFT_NOVEL("99", "습작")
     ;
 
     private final String code;
